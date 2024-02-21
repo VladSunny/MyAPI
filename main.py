f9ef5222c897d62b11cd972b8372e99ca90ce5cd
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-21 22:59:52 +0300

    Hello name

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from fastapi import FastAPI, Query
+from typing import List
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = FastAPI()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/hello/")
+def read_name(name: str):
+    return {"message": f"Hello {name}"}
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
