c3897296992e4aff17acd5ad06cec225ccb9cfbd
Vlad Sunny <vladislavsunny@gmail.com>
2024-02-21 23:06:23 +0300

    FIxes

--- main.py
+++ main.py
@@ -7,8 +7,3 @@ app = FastAPI()
 @app.get("/hello/")
 def read_name(name: str):
     return {"message": f"Hello {name}"}
-
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
